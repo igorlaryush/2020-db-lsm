0872c4fc8ebcf6d504f1bc60143f1bb88efcb0a6
igorl <igorlaryush@yandex.ru>
2020-05-12 23:11:41 +0500

    codeclimate fix #6

--- src/main/java/ru/mail/polis/SSTable.java
+++ src/main/java/ru/mail/polis/SSTable.java
@@ -2,16 +2,15 @@ package ru.mail.polis;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Iterator;
 import java.util.logging.Logger;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class SSTable implements Table {
 
